ecb7c984e3a013e9c6e4b79c54f289eca42aac2b
Alex Pham <67773092+apalex@users.noreply.github.com>
2024-03-13 19:48:48 -0400

    Update artificial_intelligence_learn.py

--- artificial_intelligence_learn.py
+++ artificial_intelligence_learn.py
@@ -5,7 +5,7 @@ import seaborn as sns
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import Ridge
-from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn.metrics import mean_squared_error, mean_absolute_error, mean_absolute_percentage_error, classification_report, confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow import keras
@@ -463,32 +463,44 @@ from keras.callbacks import EarlyStopping
 # losses.plot()
 # plt.show()
 
-df = pd.read_csv('cancer_classification.csv')
-print(df.describe().transpose())
+# df = pd.read_csv('cancer_classification.csv')
+# print(df.describe().transpose())
 
-sns.countplot(x='benign_0__mal_1',data=df)
-plt.show()
+# sns.countplot(x='benign_0__mal_1',data=df)
+# plt.show()
 
-print(df.corr()['benign_0__mal_1'].sort_values().plot(kind='bar'))
-plt.show()
+# print(df.corr()['benign_0__mal_1'].sort_values().plot(kind='bar'))
+# plt.show()
 
-plt.figure(figsize=(12,12))
-sns.heatmap(df.corr())
-plt.show()
+# plt.figure(figsize=(12,12))
+# sns.heatmap(df.corr())
+# plt.show()
 
-X = df.drop('benign_0__mal_1',axis=1).values
-y = df['benign_0__mal_1'].values
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=101)
-scaler = MinMaxScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)
+# X = df.drop('benign_0__mal_1',axis=1).values
+# y = df['benign_0__mal_1'].values
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=101)
+# scaler = MinMaxScaler()
+# X_train = scaler.fit_transform(X_train)
+# X_test = scaler.transform(X_test)
 
-model = Sequential()
-model.add(Dense(30,activation='relu'))
-model.add(Dense(15,activation='relu'))
-model.add(Dense(1,activation='sigmoid'))
-model.compile(loss='binary_crossentropy',optimizer='adam')
-model.fit(x=X_train,y=y_train,epochs=600,validation_data=(X_test,y_test))
-losses = pd.DataFrame(model.model.history)
-losses.plot()
-plt.show()
+# model = Sequential()
+# model.add(Dense(30,activation='relu'))
+# model.add(Dropout(0.5))
+# model.add(Dense(15,activation='relu'))
+# model.add(Dense(1,activation='sigmoid'))
+# model.compile(loss='binary_crossentropy',optimizer='adam')
+# early_stop = EarlyStopping(monitor='val_loss',mode='min',verbose=1,patience=25)
+# model.fit(x=X_train,y=y_train,epochs=600,validation_data=(X_test,y_test),callbacks=[early_stop])
+
+# losses = pd.DataFrame(model.history.history)
+# losses.plot()
+# plt.show()
+
+# predictions = (model.predict(X_test) > 0.5).astype(int)
+# print(classification_report(y_test, predictions))
+# print(confusion_matrix(y_test,predictions))
+
+
+
+
+
